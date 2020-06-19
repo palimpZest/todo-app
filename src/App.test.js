8872b35a591ec24459394551f5ca03fc10765607
palimpZest <polo.vaca365@gmail.com>
2020-06-19 11:45:13 +0200

    feat: Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,91 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import {
+  mockedTodos,
+  itemToAdd,
+  itemToRemoveId,
+  itemToUpdateId,
+  titleToUpdate,
+} from './mocks';
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('TODO tests', () => {
+  test('should add todo item', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.concat(itemToAdd);
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(newTodos.length).toBe(6);
+  });
+
+  test('should update todo item', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.map((item) => {
+      if (item.id === itemToUpdateId) {
+        return {
+          ...item,
+          title: titleToUpdate,
+        };
+      }
+      return item;
+    });
+
+    expect(copiedMockedTodos[2].title).toBe('Bonjour');
+    expect(newTodos[2].title).toBe(titleToUpdate);
+  });
+
+  test('should remove todo item', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.filter(
+      (item) => item.id !== itemToRemoveId,
+    );
+    const findRemoveditem = newTodos.find((item) => item.id === itemToRemoveId);
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(findRemoveditem).toBe(undefined);
+    expect(newTodos.length).toBe(4);
+  });
+
+  test('should display all items', () => {
+    const copiedMockedTodos = [...mockedTodos];
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(copiedMockedTodos.length).toBe(mockedTodos.length);
+  });
+
+  test('should display all active items', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.filter(
+      (item) => item.completed === false,
+    );
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(newTodos.length).toBe(2);
+  });
+
+  test('should display all completed items', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.filter(
+      (item) => item.completed === true,
+    );
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(newTodos.length).toBe(3);
+  });
+
+  test('should clear completed items', () => {
+    const copiedMockedTodos = [...mockedTodos];
+    const newTodos = copiedMockedTodos.filter(
+      (item) => item.completed === false,
+    );
+
+    expect(copiedMockedTodos.length).toBe(5);
+    expect(newTodos.length).toBe(2);
+  });
 });
---
+++ src/mocks/index.js
@@ -0,0 +1,37 @@
+export const mockedTodos = [
+  {
+    id: 'f2987ca6-19ec-4749-a8e4-87849f35c4a3',
+    title: 'Hey',
+    completed: false,
+  },
+  {
+    id: 'a243277e-5e22-47cc-aafb-f53ccbf22708',
+    title: 'Hello',
+    completed: true,
+  },
+  {
+    id: 'b6085f61-37f8-40dc-9112-a480d6e2040a',
+    title: 'Bonjour',
+    completed: true,
+  },
+  {
+    id: 'd8946dd3-71b6-434e-8372-480618d511b0',
+    title: 'Hola',
+    completed: false,
+  },
+  {
+    id: '296e6558-bb7a-4ecb-9f18-1b4a496211b2',
+    title: "Kon'nichiwa",
+    completed: true,
+  },
+];
+
+export const itemToAdd = {
+  id: 'abc12def-19az-0000-a5e4-87849f35erty',
+  title: 'new todo title',
+  completed: false,
+};
+
+export const itemToRemoveId = 'f2987ca6-19ec-4749-a8e4-87849f35c4a3';
+export const itemToUpdateId = 'b6085f61-37f8-40dc-9112-a480d6e2040a';
+export const titleToUpdate = 'Hello again';
